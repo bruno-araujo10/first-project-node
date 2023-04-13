6dfc5641726d52834020febbbaa1dac5daf067ad
bruno-araujo10 <brunoaraujo.developer@gmail.com>
2023-04-13 18:36:08 -0300

    updated port

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+
 const express = require('express')
 
 const uuid = require('uuid')
 
-const port = 3000
+const cors = require("cors")
+
+const port = 3001
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`🏴‍☠️ Server started on port ${port}`)
